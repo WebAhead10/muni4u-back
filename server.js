fa9d6539e84efa21fa34675aa3ebb494dc1513eb
Elias <elias.29@hotmail.com>
2021-08-23 19:10:40 +0300

    fix fetch issue that happend on react

--- server.js
+++ server.js
@@ -5,14 +5,31 @@ const cors = require("cors");
 const mailer = require("nodemailer");
 const cookie = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 4000;
 
-app.use(cors("cors"));
+// app.use(cors("cors"));
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+app.options(
+  '*',
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 // app.use("jwt");
 app.use(express.urlencoded({extended: false}))
 app.use(router);
 
-const port = process.env.PORT || 4000;
+
 app.listen(port, () =>
   console.log(`Server listening on http://localhost:${port}`)
 );
